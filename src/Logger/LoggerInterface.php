2f62d05ae487c0290df8cfad823ced85ac9dcceb
Bert Hekman <bert@pbwebmedia.com>
2018-04-25 16:09:37 +0200

    Fix: TraceableBrowser now compatible with newer version of Buzz which uses PSR7 requests and responses

--- src/Logger/LoggerInterface.php
+++ src/Logger/LoggerInterface.php
@@ -4,6 +4,8 @@ namespace Pbweb\BuzzBundle\Logger;
 
 use Buzz\Message\MessageInterface;
 use Buzz\Message\RequestInterface;
+use Psr\Http\Message\RequestInterface as Psr7RequestInterface;
+use Psr\Http\Message\ResponseInterface as Psr7ResponseInterface;
 
 /**
  * @copyright 2015 PB Web Media B.V.
@@ -11,7 +13,9 @@ use Buzz\Message\RequestInterface;
 interface LoggerInterface
 {
     public function start(RequestInterface $request);
+    public function startRequest(Psr7RequestInterface $request);
     public function stop(MessageInterface $response);
+    public function stopRequest(Psr7ResponseInterface $response);
     public function fail(\Exception $exception);
     public function clear();
 }
