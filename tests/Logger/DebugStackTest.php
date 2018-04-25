bbb5c162108adaf6c4a2ef994fff3e2ac9e7e2cd
Bert Hekman <bert@pbwebmedia.com>
2018-04-25 16:27:31 +0200

    Add some tests

--- tests/Logger/DebugStackTest.php
+++ tests/Logger/DebugStackTest.php
@@ -4,6 +4,7 @@ namespace Pbweb\BuzzBundle\Logger;
 
 use Buzz\Message\Request;
 use Buzz\Message\Response;
+use Mockery\Mock;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Stopwatch\Stopwatch;
 
@@ -14,71 +15,49 @@ class DebugStackTest extends TestCase
 {
     /** @var DebugStack */
     private $stack;
-
-    /** @var \PHPUnit_Framework_MockObject_MockObject|Stopwatch */
+    /** @var Mock|Stopwatch */
     private $stopwatch;
+    /** @var Mock|Request */
+    private $requestA;
+    /** @var Mock|Request */
+    private $requestB;
+    /** @var Mock|Response */
+    private $response;
 
     protected function setUp()
     {
-        $this->stopwatch = $this->createMockStopwatch();
+        $this->stopwatch = \Mockery::mock(Stopwatch::class);
+        $this->stopwatch->shouldIgnoreMissing();
         $this->stack = new DebugStack($this->stopwatch);
+
+        $this->requestA = \Mockery::mock(Request::class);
+        $this->requestB = \Mockery::mock(Request::class);
+        $this->response = \Mockery::mock(Response::class);
     }
 
     public function test()
     {
-        $requestA = $this->createMockRequest();
-        $responseA = $this->createMockResponse();
+        $this->stack->start($this->requestA);
+        $this->stack->stop($this->response);
 
-        $this->stack->start($requestA);
-        $this->stack->stop($responseA);
+        $exception = new \Exception('Failed');
 
-        $requestB = $this->createMockRequest();
-        $exceptionB = new \Exception('Failed');
-
-        $this->stack->start($requestB);
-        $this->stack->fail($exceptionB);
+        $this->stack->start($this->requestB);
+        $this->stack->fail($exception);
 
         $result = $this->stack->getStack();
 
         $this->assertInternalType('array', $result);
         $this->assertCount(2, $result);
 
-        $this->assertSame($requestA, $result[1]['request']);
-        $this->assertSame($responseA, $result[1]['response']);
+        $this->assertSame($this->requestA, $result[1]['request']);
+        $this->assertSame($this->response, $result[1]['response']);
         $this->assertNull($result[1]['exception']);
         $this->assertInternalType('float', $result[1]['executionTime']);
 
-        $this->assertSame($requestB, $result[2]['request']);
+        $this->assertSame($this->requestB, $result[2]['request']);
         $this->assertNull($result[2]['response']);
-        $this->assertSame((string) $exceptionB, $result[2]['exception']);
+        $this->assertSame((string) $exception, $result[2]['exception']);
         $this->assertInternalType('float', $result[2]['executionTime']);
-    }
-
-    /**
-     * @return \PHPUnit_Framework_MockObject_MockObject|Stopwatch
-     */
-    private function createMockStopwatch()
-    {
-        return $this->createMock(Stopwatch::class);
-    }
-
-    /**
-     * @return \PHPUnit_Framework_MockObject_MockObject|Request
-     */
-    private function createMockRequest()
-    {
-        return $this->getMockBuilder(Request::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-    }
-
-    /**
-     * @return \PHPUnit_Framework_MockObject_MockObject|Response
-     */
-    private function createMockResponse()
-    {
-        return $this->getMockBuilder(Response::class)
-            ->disableOriginalConstructor()
-            ->getMock();
     }
 }
